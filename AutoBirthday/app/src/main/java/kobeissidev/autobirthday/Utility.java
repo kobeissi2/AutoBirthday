005873983e3c232954e46ff944a080fa8d21a6f5
Mohamad Kobeissi <mohamadk@umich.edu>
2017-09-27 14:45:10 -0400

    API 21 Support Added.

--- AutoBirthday/app/src/main/java/kobeissidev/autobirthday/Utility.java
+++ AutoBirthday/app/src/main/java/kobeissidev/autobirthday/Utility.java
@@ -1,19 +1,24 @@
 package kobeissidev.autobirthday;
 
-import android.annotation.TargetApi;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.content.Context;
+import android.os.Build;
 
-//@TargetApi(23)
 public class Utility {
     public static void scheduleJob(Context context) {
         ComponentName serviceComponent = new ComponentName(context, MyJobService.class);
+        JobScheduler jobScheduler;
         JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponent);
         builder.setMinimumLatency(60000);
         builder.setOverrideDeadline(60000);
-        JobScheduler jobScheduler = context.getSystemService(JobScheduler.class);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            jobScheduler = context.getSystemService(JobScheduler.class);
+        } else {
+            jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        }
         jobScheduler.schedule(builder.build());
     }
 }
